65f73e0351061469057e48725ddef9203fe3a363
Manal Soava <manal.hibat.allah@gmail.com>
2022-09-27 14:55:41 +0200

    code javaScript mise en place

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// Menu bars au responsive
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -20,61 +21,67 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//----la fonctionnalité du bouton (x) close----
-//Recupérer le button du DOM
-const btnClose = document.querySelector(".close");
-//au click en ajoute le style display="none"
-btnClose.addEventListener("click", () => {
-  modalbg.style.display = "none";
-});
+// La fonctionnalité du bouton (x) et (Fermer)
+// Recupérer les buttons du DOM
 
-//---Valider les champs du formulaire----
+const btnClose = document.querySelectorAll(".btn-x");
+// au click en ajoute le style display="none"
+btnClose.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    modalbg.style.display = "none";
+    // Relancer le formulaire
+    document.querySelector(".modal-body").style.display = "block";
+    // caché le message de validation
+    document.querySelector(".validation").style.display = "none";
+  })
+);
 
-// Nom et prenom
-//fonction pour verifier les noms et prenoms
+// Valider les champs du formulaire
+
+//---Nom et prenom---
+// fonction pour tester la validité du nom et prénom
 const validName = (test) => {
-  //creer le regEx qui verifie que le nom doit contenir minimum 2 lettres
+  // créer un regEx :le nom doit contenir minimum 2 lettres
   let name = new RegExp("^[A-Za-zéèê]{2,}$");
   return name.test(test);
 };
 
-//Récuperer input du prenom
+// Récuperer input prenom du DOM
 const prenomInput = document.querySelector("#first");
-//ecouter le mouvement en "change"
+// écouter le mouvement en "change"
 prenomInput.addEventListener("change", () => {
-  //Utilisation de la fonction validName pour verifier la valeur entrer
-  //si la fonction return false
+  // verifier la valeur entrer à l'aide de fonction validName
+  // si la fonction return false
   if (!validName(prenomInput.value)) {
-    //affichage d'un message d'erreur
+    // affichage d'un message d'erreur
     document.querySelector(".prenom-valide").innerText =
-      "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+      "Veuillez entrer 2 caractères ou plus.";
   } else {
     document.querySelector(".prenom-valide").innerText = "";
   }
 });
 
-//recuperer l'input nom
+// Refaire la meme chose pour verifier le nom
 const nomInput = document.querySelector("#last");
 nomInput.addEventListener("change", () => {
-  //verrifier le nom avec la fonction valid name
   if (!validName(nomInput.value)) {
     document.querySelector(".nom-valide").innerText =
-      "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+      "Veuillez entrer 2 caractères ou plus.";
   } else {
-    document.querySelector(".prenom-valide").innerText = "";
+    document.querySelector(".nom-valide").innerText = "";
   }
 });
 
-// Email
-//fonction verrifie l'email
+//---Email---
+// fonction por tester la validité de l'email
 const validEmail = (test) => {
-  //creer un regEx pour la validation des emails
-  //Le regEx utiliser et récuperer du site regexr.com
+  // creer un regEx pour la validation des emails
+  // Le regEx utiliser et récuperer du site regexr.com
   const email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
   return email.test(test);
 };
 
-//recuperer l'inpt email
+// verrifier la valeur entrer
 const emailInput = document.querySelector("#email");
 emailInput.addEventListener("change", () => {
   if (!validEmail(emailInput.value)) {
@@ -85,31 +92,31 @@ emailInput.addEventListener("change", () => {
   }
 });
 
-//verrifier la validité de la date de naissance
+//---la date de naissance---
 const valideBirth = (test) => {
-  const birthday = new RegExp(
-    "^(([19][0-9]{2})|([200][1-4]))(-)(((0)[0-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1])$"
-  );
+  //regex accepte les date de type aaaa-mm-jj ou aaaa/mm/jj
+  const birthday =
+    /19[0-9]{2}|200[01234]{1}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])*/;
   return birthday.test(test);
 };
 
 const birthInput = document.querySelector("#birthdate");
 birthInput.addEventListener("change", () => {
   console.log(birthInput.value);
-  if (!valideBirth(birthInput.value)) {
+  if (!birthInput.value || !valideBirth(birthInput.value)) {
     document.querySelector(".birth-valide").innerText =
-      "Vous devez entrer une date de naissance valide.";
+      "Vous devez entrer une date de naissance valide '18ans ou plus'.";
   } else {
     document.querySelector(".birth-valide").innerText = "";
   }
 });
 
-//le champs de quantité
-//recuperer element de la DOM
+//---nombres de tournois---
+// recuperer element de la DOM
 const quantityInput = document.querySelector("#quantity");
-//ecouter le mouvement en "change"
+// ecouter le mouvement en "change"
 quantityInput.addEventListener("change", () => {
-  //si il n'a aucun valeur entrer
+  // si il n'a aucun valeur entrer
   if (!quantityInput.value) {
     document.querySelector(".quantity-valide").innerText =
       "Veuillez entrer un chiffre";
@@ -118,33 +125,24 @@ quantityInput.addEventListener("change", () => {
   }
 });
 
-// City
+//---City---
 //recuperer les inputs radio
 const cityChecked = document.querySelectorAll("[name=location]");
-let cityValid = Boolean;
-for (let city of cityChecked) {
+let cityValid = "";
+cityChecked.forEach((city) =>
   city.addEventListener("change", () => {
-    if (!city.value) {
-      cityValid = false;
-      console.log(cityValid);
-    } else {
-      cityValid = true;
-      console.log(cityValid);
+    if (city.value) {
+      cityValid = city.value;
     }
-  });
-  !cityValid
-    ? (document.querySelector(".city-valide").innerText =
-        "Veuillez choisir une ville")
-    : (document.querySelector(".city-valide").innerText = "");
-}
-
+  })
+);
 // Condition generale
-//recuperer element
+// recuperer element
 const condition = document.querySelector("#checkbox1");
 condition.addEventListener("change", () => {
-  //si la case n'est pas coché
+  // si la case n'est pas coché
   if (condition.checked !== true) {
-    //afficher un message d'erreur
+    // afficher un message d'erreur
     document.querySelector(".condition").innerText =
       "Veuillez accépter les conditions d'utilisation";
   } else {
@@ -152,32 +150,59 @@ condition.addEventListener("change", () => {
   }
 });
 
-//----envoi du donner au localestorage----
+// Si utilisateur souhaite etre prevenu au prochain evenement
+// creer un varriable boolean qui prend la valeur false par defaut
+let prevenu = false;
+const utilisateurPrevenu = document.querySelector("#checkbox2");
+utilisateurPrevenu.addEventListener("change", () => {
+  // si l'element est coché la varriable prend la valeur true
+  if (utilisateurPrevenu.checked === true) {
+    prevenu = true;
+  }
+});
 
+// Envoi du donner au localestorage + affichage du message de validation
+// Récuperer element (form)
 const submitForm = document.querySelector("[name=reserve]");
+// Ecouter l'evenemnt en "Submit"
 submitForm.addEventListener("submit", (e) => {
+  // Empecher la gestion du submit par defaut
   e.preventDefault();
+  // si tous les champs sont valid
   if (
-    // validName(prenomInput.value) &&
-    // validName(nomInput.value) &&
-    // validEmail(emailInput.value) &&
-    valideBirth(birthInput) &&
-    birthInput.value !== null //&&
-    // quantityInput.value &&
-    // cityValid &&
-    // condition.checked
+    validName(prenomInput.value) &&
+    validName(nomInput.value) &&
+    validEmail(emailInput.value) &&
+    valideBirth(birthInput.value) &&
+    quantityInput.value &&
+    cityValid &&
+    condition.checked
   ) {
     console.log("toute est bon");
-    // const player = {
-    //   prenom: prenomInput.value,
-    //   nom: nomInput.value,
-    //   email: emailInput.value,
-    //   date_aissance: birthInput.value,
-    //   nb_tournois: quantityInput.value,
-    //   ville: cityChecked,
-    // };
-    // window.localStorage.setItem("player", player);
+    // creer un objet avec les donnees saisi au formulaire
+    let player = {
+      prenom: prenomInput.value,
+      nom: nomInput.value,
+      email: emailInput.value,
+      date_aissance: birthInput.value,
+      nb_tournois: quantityInput.value,
+      ville: cityValid,
+      prochaineEvenement: prevenu,
+    };
+    console.log(player);
+    // Transformer les données en JSON
+    player = JSON.stringify(player);
+    // Sauvegarder les données au locale storage
+    window.localStorage.setItem("player", player);
+    // Caché le formulaire
+    document.querySelector(".modal-body").style.display = "none";
+    // Mise a jour du fomulaire
+    document.querySelector("[name=reserve]").reset();
+    // Afficher le message de validation
+    document.querySelector(".validation").style.display = "block";
   } else {
-    console.log("manque de champs");
+    console.log("il manque des champs");
+    // Sinon afficher une alert avec un message erreur
+    window.alert("Veillez remplir les champs qui manque");
   }
 });
